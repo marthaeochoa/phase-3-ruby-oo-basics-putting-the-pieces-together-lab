936a51ba2085e01a9125c0c0c31acfd8d44df94a
martha ochoa <marthaeochoa98@gmail.com>
2023-03-27 17:09:50 -0500

    commit tree 4060339385360399b4a376b53bfb7418d1aa5c29

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
 # Make your shoe class here!
+class Shoe
+    attr_reader :brand, :color, :size, :material, :condition
+    attr_writer :color, :size, :material, :condition
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
